81cecba6ed0aff9e11795a10d6e3dac3d754046d
wza8652wza <381147882@qq.com>
2015-06-24 09:58:24 +0800

    增加userService

---
+++ src/com/whut/wxcs/resmanager/service/impl/BaseServiceImpl.java
@@ -0,0 +1,65 @@
+package com.whut.wxcs.resmanager.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.whut.wxcs.resmanager.dao.BaseDao;
+import com.whut.wxcs.resmanager.service.BaseService;
+
+
+public abstract class BaseServiceImpl<T> implements BaseService<T> {
+
+	BaseDao<T> baseDao;
+
+	@Resource
+	public void setBaseDao(BaseDao<T> baseDao) {
+		this.baseDao = baseDao;
+	}
+
+	@Override
+	public void saveEntity(T t) {
+		baseDao.saveEntity(t);
+	}
+
+	@Override
+	public void saveOrUpdateEntity(T t) {
+		baseDao.saveOrUpdateEntity(t);
+	}
+
+	@Override
+	public void updateEntity(T t) {
+		baseDao.updateEntity(t);
+	}
+
+	@Override
+	public void deleteEntity(T t) {
+		baseDao.deleteEntity(t);
+	}
+
+	@Override
+	public void batchEntityByHql(String hql, Object... objects) {
+		baseDao.batchEntityByHql(hql, objects);
+	}
+
+	@Override
+	public T loadEntity(Integer id) {
+		return baseDao.loadEntity(id);
+	}
+
+	@Override
+	public T getEntity(Integer id) {
+		return baseDao.getEntity(id);
+	}
+
+	@Override
+	public List<T> findEntityByHql(String hql, Object... objects) {
+		return baseDao.findEntityByHql(hql, objects);
+	}
+	
+	@Override
+	public Object ubiqueResult(String hql, Object... objects) {
+		return baseDao.ubiqueResult(hql, objects);
+	}
+
+}
---
+++ src/com/whut/wxcs/resmanager/util/VaildateUtil.java
@@ -0,0 +1,23 @@
+package com.whut.wxcs.resmanager.util;
+
+import java.util.Collection;
+
+/**
+ * 校验工具类
+ * @author apple
+ *
+ */
+public class VaildateUtil {
+
+	/**
+	 * 检验是否有效
+	 * @param src
+	 */
+	public static boolean isVaild(String src){
+		return !(src==null || src.trim().equals(""));
+	}
+	public static boolean isVaild(Collection collection){
+		return !(collection==null || collection.isEmpty());
+	}
+	
+}
