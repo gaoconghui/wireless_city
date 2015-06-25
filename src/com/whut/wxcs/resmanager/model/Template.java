9b598d4fd2610531ff3c52a11241afd0ba93079b
wza8652wza <381147882@qq.com>
2015-06-25 10:23:08 +0800

    Revert "Revert a32fea5..56f2873"
    This reverts commit 5c0cd277e57e79ec0a4c038d0acd67a4a8708678.

--- src/com/whut/wxcs/resmanager/model/Template.java
+++ src/com/whut/wxcs/resmanager/model/Template.java
@@ -13,21 +13,12 @@ public class Template {
 	private Date creatTime;
 	private Set<TAttributeValue> attributes = new HashSet<TAttributeValue>();
 
-	public void addAttribute(TAttributeValue attribute) {
-		attributes.add(attribute);
-	}
-
-	public void removeAttribute(TAttributeValue attribute) {
-		if (attributes.contains(attribute))
-			attributes.remove(attribute);
-	}
-
-	public long getId() {
+	public long getTemplateId() {
 		return templateId;
 	}
 
-	public void setId(long id) {
-		this.templateId = id;
+	public void setTemplateId(long templateId) {
+		this.templateId = templateId;
 	}
 
 	public String getTemplateName() {
@@ -38,20 +29,20 @@ public class Template {
 		this.templateName = templateName;
 	}
 
-	public Catalogue getCatalogue() {
-		return catalogue;
-	}
-
-	public void setCatalogue(Catalogue catalogue) {
-		this.catalogue = catalogue;
-	}
-
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Catalogue getCatalogue() {
+		return catalogue;
+	}
+
+	public void setCatalogue(Catalogue catalogue) {
+		this.catalogue = catalogue;
 	}
 
 	public Date getCreatTime() {
@@ -62,11 +53,12 @@ public class Template {
 		this.creatTime = creatTime;
 	}
 
-	@Override
-	public String toString() {
-		return "template [id=" + templateId + ", templateName=" + templateName
-				+ ", description=" + description + ", catalogue=" + catalogue
-				+ ", creatTime=" + creatTime + "]";
+	public Set<TAttributeValue> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(Set<TAttributeValue> attributes) {
+		this.attributes = attributes;
 	}
 
 }
