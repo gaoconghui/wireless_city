9da65a1d89d9eae24ebbf7983392df7d18c20b1e
wza8652wza <381147882@qq.com>
2015-06-26 12:38:14 +0800

    Revert "s"
    Conflicts:
    	.gitignore
    	WebContent/admin/cataloguemanager.jsp
    	WebContent/admin/designcatalogue.jsp
    	WebContent/login/provider_reg_success.jsp
    	build/classes/applicationContext.xml
    
    build/classes/com/whut/wxcs/resmanager/bean/test/testCatalogueService.cl
    ass
    	build/classes/com/whut/wxcs/resmanager/dao/impl/BaseDaoImpl.class
    	build/classes/com/whut/wxcs/resmanager/model/Catalogue.class
    	build/classes/com/whut/wxcs/resmanager/model/Provider.class
    	build/classes/com/whut/wxcs/resmanager/model/Provider.hbm.xml
    	build/classes/com/whut/wxcs/resmanager/model/Template.class
    	build/classes/com/whut/wxcs/resmanager/service/CatalogueService.class
    
    build/classes/com/whut/wxcs/resmanager/service/impl/BaseServiceImpl.clas
    s
    
    build/classes/com/whut/wxcs/resmanager/service/impl/CatalogueServiceImpl
    .class
    	build/classes/struts.xml
    	config/applicationContext.xml
    	config/struts.xml
    	src/com/whut/wxcs/resmanager/dao/impl/CatalogueDaoImpl.java
    	src/com/whut/wxcs/resmanager/model/Catalogue.java
    	src/com/whut/wxcs/resmanager/model/Provider.hbm.xml
    	src/com/whut/wxcs/resmanager/model/Provider.java
    	src/com/whut/wxcs/resmanager/model/Template.java
    	src/com/whut/wxcs/resmanager/service/CatalogueService.java
    	src/com/whut/wxcs/resmanager/service/impl/CatalogueServiceImpl.java
    	test/com/whut/wxcs/resmanager/bean/test/testCatalogueService.java

--- src/com/whut/wxcs/resmanager/model/Template.java
+++ src/com/whut/wxcs/resmanager/model/Template.java
@@ -13,30 +13,12 @@ public class Template {
 	private Date createTime;
 	private Set<Attribute> attributes = new HashSet<Attribute>();
 
-<<<<<<< HEAD
 	public long getId() {
 		return id;
 	}
 
 	public void setId(long id) {
 		this.id = id;
-=======
-	public void addAttribute(TAttributeValue attribute) {
-		attributes.add(attribute);
-	}
-
-	public void removeAttribute(TAttributeValue attribute) {
-		if (attributes.contains(attribute))
-			attributes.remove(attribute);
-	}
-
-	public long getId() {
-		return templateId;
-	}
-
-	public void setId(long id) {
-		this.templateId = id;
->>>>>>> User&Provider_COPY
 	}
 
 	public String getTemplateName() {
@@ -47,18 +29,6 @@ public class Template {
 		this.templateName = templateName;
 	}
 
-	public Catalogue getCatalogue() {
-		return catalogue;
-	}
-
-	public void setCatalogue(Catalogue catalogue) {
-		this.catalogue = catalogue;
-	}
-
-<<<<<<< HEAD
-	public Date getCreateTime() {
-		return createTime;
-=======
 	public String getDescription() {
 		return description;
 	}
@@ -67,29 +37,28 @@ public class Template {
 		this.description = description;
 	}
 
-	public Date getCreatTime() {
-		return creatTime;
->>>>>>> User&Provider_COPY
+	public Catalogue getCatalogue() {
+		return catalogue;
+	}
+
+	public void setCatalogue(Catalogue catalogue) {
+		this.catalogue = catalogue;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
 	}
 
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
 
-<<<<<<< HEAD
 	public Set<Attribute> getAttributes() {
 		return attributes;
 	}
 
 	public void setAttributes(Set<Attribute> attributes) {
 		this.attributes = attributes;
-=======
-	@Override
-	public String toString() {
-		return "template [id=" + templateId + ", templateName=" + templateName
-				+ ", description=" + description + ", catalogue=" + catalogue
-				+ ", creatTime=" + creatTime + "]";
->>>>>>> User&Provider_COPY
 	}
 
 }
